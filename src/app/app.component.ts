934e32be789662cf23cf5ed65f9af20f28b64818
Aleks <nyc11374@gmail.com>
2024-11-10 13:51:45 -0500

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,7 @@ export class AppComponent {
   message = '';
 
   constructor(private http: HttpClient) {
-    this.http.get('/api/message')
+    this.http.get('https://pokerleagueproj.azurewebsites.net/api/users?code=c_yUI2yu9enkqu-5SaiSUKZlZWTH-nVlPGpz1kye-UkBAzFuPp3f_g%3D%3D')
       .subscribe((resp: any) => this.message = resp.text);
   }
 }
