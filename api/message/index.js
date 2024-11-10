7c964917f98f786b16a65d39090de185a670d63b
Aleks <nyc11374@gmail.com>
2024-11-10 14:03:10 -0500

    test 3

--- api/message/index.js
+++ api/message/index.js
@@ -1,20 +1,10 @@
-const fetch = require('node-fetch');
-
 module.exports = async function (context, req) {
-    try {
-        console.log("debugging");
-        const response = await fetch('https://pokerleagueproj.azurewebsites.net/api/users?code=c_yUI2yu9enkqu-5SaiSUKZlZWTH-nVlPGpz1kye-UkBAzFuPp3f_g%3D%3D');
-        const data = await response.json();
-        console.log(data);
-        
-        context.res = {
-            status: 200,
-            body: data
-        };
-    } catch (error) {
-        context.res = {
-            status: 500,
-            body: { error: 'Failed to fetch data from API' }
-        };
-    }
+    console.log('JavaScript HTTP trigger function processed a request.');
+    console.log('Request headers:', req.headers);
+    console.log('Request body:', req.body);
+    //display the json from the api call
+
+    context.res.json({
+        text: "Hello from the API"
+    });
 };
