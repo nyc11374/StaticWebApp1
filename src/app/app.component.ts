c9dcec7d7eeccf139ba8432fdc1cc4f7efb914e9
Aleks <nyc11374@gmail.com>
2024-11-10 14:11:34 -0500

    test 4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,16 @@
 import { HttpClient } from '@angular/common/http';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   template: `<div>{{message}}</div>`,
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   message = '';
 
-  
-  constructor(private http: HttpClient) {
-    console.log('Hello World!');
-  
-
-    this.http.get('https://pokerleagueproj.azurewebsites.net/api/users?code=c_yUI2yu9enkqu-5SaiSUKZlZWTH-nVlPGpz1kye-UkBAzFuPp3f_g%3D%3D')
-      .subscribe((resp: any) => this.message = resp.text);
-
-      console.log('get finished in app.components!');
-  
-  }
+  constructor(private http: HttpClient) { console.log('Hello World!'); }
+  async ngOnInit() { await this.loadData(); console.log('get finished in app.components!'); }
+  async loadData() { try { 
+    const resp: any = await this.http.get('https://pokerleagueproj.azurewebsites.net/api/users?code=c_yUI2yu9enkqu-5SaiSUKZlZWTH-nVlPGpz1kye-UkBAzFuPp3f_g%3D%3D').toPromise(); 
+    this.message = resp.text; } catch (error) { console.error('Error fetching data', error); } }
 }
