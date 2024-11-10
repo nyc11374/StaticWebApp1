18d28be1a5c60599c960660b0f8d6b71b688668b
Aleks <nyc11374@gmail.com>
2024-11-10 13:20:43 -0500

    adding external api

--- api/message/index.js
+++ api/message/index.js
@@ -1,5 +1,18 @@
+const fetch = require('node-fetch');
+
 module.exports = async function (context, req) {
-    context.res.json({
-        text: "Hello from the API 1"
-    });
+    try {
+        const response = await fetch('https://pokerleagueproj.azurewebsites.net/api/users?code=c_yUI2yu9enkqu-5SaiSUKZlZWTH-nVlPGpz1kye-UkBAzFuPp3f_g%3D%3D');
+        const data = await response.json();
+
+        context.res = {
+            status: 200,
+            body: data
+        };
+    } catch (error) {
+        context.res = {
+            status: 500,
+            body: { error: 'Failed to fetch data from API' }
+        };
+    }
 };
