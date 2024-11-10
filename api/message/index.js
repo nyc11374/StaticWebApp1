2f7781f57dc1f8e0429d62d2c00337fa5b1e00d5
Aleks <nyc11374@gmail.com>
2024-11-10 13:57:19 -0500

    test 1

--- api/message/index.js
+++ api/message/index.js
@@ -2,9 +2,11 @@ const fetch = require('node-fetch');
 
 module.exports = async function (context, req) {
     try {
+        console.log("debugging");
         const response = await fetch('https://pokerleagueproj.azurewebsites.net/api/users?code=c_yUI2yu9enkqu-5SaiSUKZlZWTH-nVlPGpz1kye-UkBAzFuPp3f_g%3D%3D');
         const data = await response.json();
-
+        console.log(data);
+        
         context.res = {
             status: 200,
             body: data
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,8 +8,15 @@ import { Component } from '@angular/core';
 export class AppComponent {
   message = '';
 
+  
   constructor(private http: HttpClient) {
+    console.log('Hello World!');
+  
+
     this.http.get('https://pokerleagueproj.azurewebsites.net/api/users?code=c_yUI2yu9enkqu-5SaiSUKZlZWTH-nVlPGpz1kye-UkBAzFuPp3f_g%3D%3D')
       .subscribe((resp: any) => this.message = resp.text);
+
+      console.log('get finished in app.components!');
+  
   }
 }
